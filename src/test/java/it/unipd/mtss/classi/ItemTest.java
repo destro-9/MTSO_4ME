965bc33ec79eadb3e1b0cf9e643c17f9129cd60b
UraganoAngelina <alberto.angeloni@studenti.unipd.it>
2022-05-22 16:32:43 +0200

    EItem test

--- src/test/java/it/unipd/mtss/classi/ItemTest.java
+++ src/test/java/it/unipd/mtss/classi/ItemTest.java
@@ -1,5 +1,18 @@
 package it.unipd.mtss.classi;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class ItemTest {
-    
+    @Test(expected = IllegalArgumentException.class)
+    public void PrezzoMinoreDiZero(){new EItem(EItem.itemType.Processor, "I7"  , -3.0);}
+    @Test
+    public void OggettoValido(){new EItem(EItem.itemType.Processor, "I7"  , 45.50);}
+    @Test
+    public void RitornaNomeCorretto(){EItem e=new EItem(EItem.itemType.Processor, "I7"  , 45.50); assertEquals("I7", e.getNome());}
+    @Test
+    public void RitornaTipoCorretto(){EItem e=new EItem(EItem.itemType.Processor, "I7"  , 45.50); assertEquals(EItem.itemType.Processor, e.getTipo());}
+    @Test
+    public void RitornaPrezzoCorretto(){EItem e=new EItem(EItem.itemType.Processor, "I7"  , 45.50); assertEquals(45.50, e.getPrezzo(),0.001);}
 }
