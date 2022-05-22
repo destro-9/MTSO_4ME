bce86e2167b1a9ea3e5d828ce9c6faaa94cada15
UraganoAngelina <alberto.angeloni@studenti.unipd.it>
2022-05-22 16:10:11 +0200

    user test

--- src/test/java/it/unipd/mtss/classi/UserTest.java
+++ src/test/java/it/unipd/mtss/classi/UserTest.java
@@ -6,4 +6,16 @@ import static org.junit.Assert.assertEquals;
 public class UserTest {
     @Test
     public void ValoriNomaliVannoAccettati(){new User(0,40,"Mario","Rossi");}
+    @Test(expected=IllegalArgumentException.class)
+    public void IDmaggioreDiZero(){new User(-1,33,"gesu","nazareno");}
+    @Test(expected = IllegalArgumentException.class)
+    public void EtaMinorediZero(){new User(2,-44, "angelo", "azzurro");}
+    @Test
+    public void NomeCorretto(){User u = new User(0,40,"Mario","Rossi"); assertEquals("Mario", u.getName());}
+    @Test
+    public void IdCorretto(){User u = new User(0,40,"Mario","Rossi"); assertEquals(0, u.getId());}
+    @Test
+    public void EtaCorretta(){User u = new User(0,40,"Mario","Rossi"); assertEquals(40, u.getEta());}
+    @Test
+    public void CognomeCorretto(){User u = new User(0,40,"Mario","Rossi"); assertEquals("Rossi", u.getSurname());}
 }
