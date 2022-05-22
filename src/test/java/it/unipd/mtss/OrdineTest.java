eb5e074d1a1d9825193219f1bd857b24f207e984
UraganoAngelina <alberto.angeloni@studenti.unipd.it>
2022-05-22 17:01:51 +0200

    costruttore ordine test

--- src/test/java/it/unipd/mtss/OrdineTest.java
+++ src/test/java/it/unipd/mtss/OrdineTest.java
@@ -1,5 +1,42 @@
 package it.unipd.mtss;
 
+import org.junit.Before;
+import org.junit.Test;
+import it.unipd.mtss.classi.User;
+import it.unipd.mtss.exception.OggettoNonTrovato;
+import it.unipd.mtss.exception.SgarroOrdine;
+import it.unipd.mtss.classi.EItem;
+import it.unipd.mtss.classi.EItem.itemType;
+import java.util.List;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.DateFormat;
+
 public class OrdineTest {
+    User u;
+    Date d;
+    List<EItem> l;
+    @Before
+    void Hobisognodivalori(){
+        u=new User(0, 22, "Nome", "Cognome");
+        DateFormat df=new SimpleDateFormat("HH:mm");
+        try{
+            d=df.parse("18");
+        }
+        catch(ParseException P){System.out.print("FORMATO NON VALIDO");};
+        l=Collections.emptyList();
+    }
+    @Test
+    public void OggettoValido(){
+        
+        l=Arrays.asList(new EItem(EItem.itemType.Keyboard,"A",76.0),
+            new EItem(EItem.itemType.Processor,"m1",99.0), 
+            new EItem(EItem.itemType.Motherboard,"a567",122.0));
+    }
     
 }
